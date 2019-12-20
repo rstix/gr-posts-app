a4630654b048697031a98e908e173407e4d08989
rstix <rsonroad@gmail.com>
2019-12-20 18:46:33 +0100

    posts fetched via redux

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,8 @@
 import { combineReducers } from "redux";
+import postsReducers from "./postsReducers";
 
 export default combineReducers({
-  dummy: () => "dummy"
+  posts: postsReducers
+  // dummy reducer to make it woork, to do not get errors
+  //dummy: () => "dummy"
 });

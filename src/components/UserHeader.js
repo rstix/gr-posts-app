d104283820b907a03f9dbeaa0ee72157823be8c0
rstix <rsonroad@gmail.com>
2020-01-24 13:15:33 +0100

    logic moved to mapStateToProps

--- src/components/UserHeader.js
+++ src/components/UserHeader.js
@@ -8,16 +8,17 @@ class UserHeader extends React.Component {
   }
 
   render() {
-    const user = this.props.users.find(user => user.id === this.props.userId);
+    const { user } = this.props;
     if (!user) {
       return null;
     }
-    return <div> {user.name}</div>;
+    console.log(user);
+    return <div className="header"> {user.name}</div>;
   }
 }
 
-const mapStateToProps = state => {
-  return { users: state.users };
+const mapStateToProps = (state, ownProps) => {
+  return { user: state.users.find(user => user.id === ownProps.userId) };
 };
 
 export default connect(
